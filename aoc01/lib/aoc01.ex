111ddee20c7249ab76fb043f7b16416668adf109
Steven Holdsworth <sholdsworth@gmail.com>
2020-01-30 18:36:10 +0000

    init

---
+++ aoc01/lib/aoc01.ex
@@ -0,0 +1,18 @@
+defmodule Aoc01 do
+  @moduledoc """
+  Documentation for Aoc01.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc01.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aoc01/test/aoc01_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc01Test do
+  use ExUnit.Case
+  doctest Aoc01
+
+  test "greets the world" do
+    assert Aoc01.hello() == :world
+  end
+end
