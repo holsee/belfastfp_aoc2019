6913e5b12f5cf710ba9eb224d9379a1d9107f5e3
Steven Holdsworth <sholdsworth@gmail.com>
2020-01-30 19:24:09 +0000

    AoC1 - Part 1

--- aoc01/lib/aoc01.ex
+++ aoc01/lib/aoc01.ex
@@ -3,16 +3,26 @@ defmodule Aoc01 do
   Documentation for Aoc01.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc01.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec compute_total() :: non_neg_integer()
+  def compute_total do
+    read_masses()
+    |> Enum.map(&fuel_required/1)
+    |> Enum.sum()
   end
+
+  @spec read_masses() :: [integer()]
+  def read_masses() do
+    :code.priv_dir(:aoc01)
+    |> Path.join("input.txt")
+    |> File.read!()
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @spec fuel_required(pos_integer()) :: pos_integer()
+  def fuel_required(mass) when is_integer(mass) do
+    Integer.floor_div(mass, 3) - 2
+  end
+
 end
--- aoc01/test/aoc01_test.exs
+++ aoc01/test/aoc01_test.exs
@@ -2,7 +2,30 @@ defmodule Aoc01Test do
   use ExUnit.Case
   doctest Aoc01
 
-  test "greets the world" do
-    assert Aoc01.hello() == :world
+  describe "fuel_required/1" do
+    @examples [
+      {12, 2},
+      {14, 2},
+      {1969, 654},
+      {100_756, 33583}
+    ]
+
+    test "should return fuel for mass" do
+      for {mass, expected_fuel} <- @examples do
+        assert expected_fuel == Aoc01.fuel_required(mass)
+      end
+    end
+  end
+
+  describe "read_masses" do
+    test "should return list of masses from input.txt" do
+      assert [141_657 | _] = Aoc01.read_masses()
+    end
+  end
+
+  describe "compute_total" do
+    test "should return total fuel required for massed in input.txt" do
+      assert is_integer(Aoc01.compute_total())
+    end
   end
 end
